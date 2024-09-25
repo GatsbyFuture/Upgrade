75ee61e78e5709d9111d6c9d952f8935d38b484c
Your Name <raxmatoveldor37@gmail.com>
2024-09-25 17:39:47 +0500

    message was sended to amail (SMTP) :)

--- service/auth.service.js
+++ service/auth.service.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model');
 const UserDto = require('../dtos/user.dto');
 const tokenService = require('./token.service');
+const mailSerivce = require('./mail.service');
 const bcrypt = require('bcrypt');
 
 class AuthService {
@@ -13,8 +14,10 @@ class AuthService {
 
         const hashPassword = await bcrypt.hash(password, 10);
         const user = await userModel.create({email, password: hashPassword});
-
         const userDto = new UserDto(user);
+
+        await mailSerivce.sendMail(email, `${process.env.API_URL}/api/auth/activation/${userDto.id}`);
+
         const tokens = tokenService.generateToken({...userDto});
 
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
