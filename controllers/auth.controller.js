e75e4b3fc6247a769a155fa620421c1d27213ffc
Your Name <raxmatoveldor37@gmail.com>
2024-09-26 00:09:38 +0500

    Login, logout and refresh

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,7 +5,7 @@ class AuthController {
         try {
             const {email, password} = req.body;
             const data = await authService.register(email, password);
-            res.cookie('refreshToken', data.refreshToken, {httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 });
+            res.cookie('refreshToken', data.refreshToken, {httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000});
             return res.json(data);
         } catch (e) {
             console.log(e);
@@ -17,6 +17,39 @@ class AuthController {
             const userId = req.params.id;
             await authService.activation(userId);
             return res.redirect('https://sammi.ac');
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    async login(req, res, next) {
+        try {
+            const {email, password} = req.body;
+            const data = await authService.login(email, password);
+            res.cookie('refreshToken', data.refreshToken, {httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000});
+            return res.json(data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    async logout(req, res, next) {
+        try {
+            const {refreshToken} = req.cookies;
+            const token = await authService.logout(refreshToken);
+            res.clearCookie('refreshToken');
+            return res.json({token});
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    async refresh(req, res, next) {
+        try {
+            const {refreshToken} = req.cookies;
+            const data = await authService.refresh(refreshToken);
+            res.cookie('refreshToken', data.refreshToken, {httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000});
+            res.status(200).json({message: 'The Refresh was successfully updated!', data});
         } catch (e) {
             console.log(e);
         }
--- service/token.service.js
+++ service/token.service.js
@@ -17,7 +17,32 @@ class TokenService {
         }
 
         const token = await tokenModel.create({user: userId, refreshToken});
+
         return token;
+    }
+
+    async removeToken(refreshToken) {
+        return await tokenModel.findOneAndDelete({refreshToken});
+    }
+
+    async findToken(refreshToken) {
+        return await tokenModel.findOne({refreshToken});
+    }
+
+    validateRefreshToken(token) {
+        try {
+            return jwt.verify(token, process.env.JWT_REFRESH_KEY);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    validateAccessToken(token) {
+        try {
+            return jwt.verify(token, process.env.JWT_ACCESS_KEY);
+        } catch (e) {
+            console.log(e);
+        }
     }
 }
 
